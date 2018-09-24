f63b1250e1066e8fb12f41f8ec02585ec43ce907
Dang Quoc Huy <huysamdua@yahoo.com>
2018-09-24 12:53:15 +0700

    learn oop, reg, cgi

---
+++ reg/m_vs_s.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import re
+
+# Matching Versus Searching
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'dogs', line, re.M|re.I)
+if matchObj:
+    print "match --> matchObj.group() : ", matchObj.group()
+else:
+    print "No match!!"
+
+searchObj = re.search( r'dogs', line, re.M|re.I)
+if searchObj:
+    print "search --> searchObj.group() : ", searchObj.group()
+else:
+    print "Nothing found!!"
